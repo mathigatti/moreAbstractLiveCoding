768f3f52b6268ca157ea25989fec6182000007c1
Mathias Gatti <mathigatti@gmail.com>
2022-06-23 15:44:08 -0300

    samples

--- basic.py
+++ basic.py
@@ -25,6 +25,13 @@ def ritmo_picante():
 def mas_picante():
     pass
 
+samples = {
+    "coding": ["milew1", "milew2", "milew3", "milew4"],
+    "nostalgia": ["dialup", "gativideo", "msn"],
+    "internet": ["dont_follow_rules", "alien1", "alien2", "alien3", "juan"],
+    "meditacion": ["watts1", "watts2", "watts3", "watts", "watts4", "meditacion", "olas"]
+}
+
 # ['freq', 'delay', 'buf', 'sample', 'env', 'fmod', 'pan', 'rate', 'midinote', 'channel', 'vib', 'vibdepth', 'slide', 'slidedelay', 'slidefrom', 'glide', 'glidedelay', 'bend', 'benddelay', 'coarse', 'striate', 'pshift', 'hpf', 'hpr', 'lpf', 'lpr', 'swell', 'bpf', 'bpr', 'bpnoise', 'bits', 'crush', 'dist', 'tmp', 'chop', 'tremolo', 'beat_dur', 'echo', 'echotime', 'spin', 'cut', 'room', 'mix', 'formant', 'shape', 'drive', 'sus', 'amp', 'blur', 'amplify', 'dur', 'degree', 'oct', 'bpm', 'atk', 'decay', 'rel', 'root', 'octave', 'deg', 'octa']
 
 
